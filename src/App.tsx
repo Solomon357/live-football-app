cf0ebc35eca4e90b373c82eb89c1b9abdfe9958d
Solomon357 <Solomonoddy@hotmail.com>
2024-09-05 16:01:00 +0100

    started live football app

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.scss'
+
+function App() {
+ 
+
+  
+  return (
+    <>
+     <h1>Live Football Matches</h1>
+     <p></p>
+    </>
+  )
+}
+
+export default App
